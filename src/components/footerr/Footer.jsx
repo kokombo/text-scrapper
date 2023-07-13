9038a91d2c1a1c7db34dff4cad870aa888588102
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2023-07-13 18:53:49 +0100

    updated github link

--- src/components/footerr/Footer.jsx
+++ src/components/footerr/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <p>Help</p>
 
       <a
-        href="https://theternhub.netlify.app"
+        href="https://https://github.com/BowaDAO/text-scrapper"
         aria-label="view site code on gitbub"
         target="_blank"
         rel="noreferrer"
