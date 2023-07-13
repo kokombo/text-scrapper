77775d3f8c800aa4766e80a47f26b7260a831d7e
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2023-07-13 21:08:28 +0100

    textarea size update

--- src/components/result/Result.jsx
+++ src/components/result/Result.jsx
@@ -22,7 +22,7 @@ const Result = ({ result, setResult }) => {
         <div className="flex flex-col items-start ">
           <h2 className="font-bold">Result:</h2>
           <textarea
-            className="border-[1px] border-gray-700 p-2 w-[400px] max-w-full h-[300px] "
+            className="border-[1px] border-gray-700 p-2 w-[380px] max-w-full h-[300px] "
             defaultValue={result}
           />
 
